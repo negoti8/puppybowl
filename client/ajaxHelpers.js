fb9fccbdb26f905624c4b1ec2613dbb0512b0961
brian.kim <brianhkim@gmail.com>
2023-03-23 10:44:29 -0500

    uploaded players in puppy bowl

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,11 +1,11 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
 const cohortName = "2207-FTB-ET-WEB-PT";
 // Use the APIURL variable for fetch requests
-const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/2207-FTB-ET-WEB-PT/players`;
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
 
 export const fetchAllPlayers = async () => {
   try {
-    const response = await fetch(`${APIURL}`);
+    const response = await fetch(`${APIURL}/players`);
     const result = await response.json();
     if (result.error) {
       throw result.error;
